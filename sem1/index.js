c5af75d672a99bff6016c657ffb25057e6babe77
Luisa Vlad <luisavlad15@gmail.com>
2025-10-06 23:06:43 +0300

    seminar introductiv

---
+++ sem1/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.get("/server/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(8080);
